d5aa459b1e704e6856bc5eb1fe33fc1ba132103a
Ilija Vukotic <ivukotic@cern.ch>
2018-08-29 15:13:00 -0700

    added create_all

---
+++ kube/create_all.bat
@@ -0,0 +1,13 @@
+kubectl create -f ./namespace.yaml
+secrets/google-secret.bat
+kubectl create -f  ./secrets/mailgun-secret.yaml
+kubectl create -f ./es-cluster-state.yaml
+kubectl create -f ./frontier-failed-q.yaml
+kubectl create -f ./frontier-threads.yaml
+kubectl create -f ./fts-aggregator.yaml
+kubectl create -f ./job-task-indexing.yaml
+kubectl create -f ./packet-loss.yaml
+kubectl create -f ./ps-indexing.yaml
+kubectl create -f ./sending-mails.yaml
+kubectl create -f ./top-users.yaml
+kubectl create -f ./user-reports.yaml
