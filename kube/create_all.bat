4cab14549bf5cf62dda01ea0e1448e963661a64c
Ilija Vukotic <ivukotic@cern.ch>
2018-09-18 14:34:15 -0500

    update.

--- kube/create_all.bat
+++ kube/create_all.bat
@@ -11,3 +11,6 @@ kubectl create -f ./ps-indexing.yaml
 kubectl create -f ./sending-mails.yaml
 kubectl create -f ./top-users.yaml
 kubectl create -f ./user-reports.yaml
+cd ..\..\containers\
+kubectl create -f ./FTS-secret.yaml
+kubectl create -f ./FTS-deployment.yaml
