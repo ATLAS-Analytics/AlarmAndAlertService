eb63e122454467fe0df38bf806156c951da2fa8d
Ilija Vukotic <ivukotic@cern.ch>
2019-11-25 11:40:48 -0600

    fix

--- es-make-backup.py
+++ es-make-backup.py
@@ -11,8 +11,9 @@ import json
 with open('/config/config.json') as json_data:
     config = json.load(json_data,)
 
-ES_CONN = 'http://' + config['ES_USER'] + ':' + config['ES_PASS'] + '@' + config['ES_HOST'] + ':9200/'
+ES_CONN = 'http://' + config['ES_USER'] + ':' + config['ES_PASS'] + '@' + config['ES_HOST'] + ':9200'
 command = ES_CONN + '/_snapshot/my_backup/auto-' + today.strftime("%Y-%m-%d")
+print("command:", command)
 r = requests.put(command)
 res = r.json()
 print(res)
