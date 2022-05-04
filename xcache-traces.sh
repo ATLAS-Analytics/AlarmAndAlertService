43c719f84d4b828d628d1008cf6547bd2fe0cc18
Ilija Vukotic <ivukotic@cern.ch>
2022-05-04 16:38:10 -0500

    proxy checking

--- xcache-traces.sh
+++ xcache-traces.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 date
 
+# check for proxy and export it.
+
+export X509_USER_PROXY=/etc/proxy/x509up
+
+while [ ! -f $X509_USER_PROXY ]
+do
+  sleep 10
+  echo "waiting for x509 proxy."
+done
+
+ls -lh $X509_USER_PROXY
+
 python3 xcache-traces.py
 rc=$?; if [[ $rc != 0 ]]; then 
-    echo "problem checking for bad frontier sql. Exiting."
+    echo "problem checking rucio traces. Exiting."
     exit $rc
 fi
