c930a10f8e13df2c5fc0a0057bf59c972bd4ea53
Ilija Vukotic <ivukotic@cern.ch>
2022-05-05 21:56:26 -0500

    devs

--- xcache-traces.py
+++ xcache-traces.py
@@ -4,20 +4,27 @@
 # ====
 
 
-#  needs a valid proxy.
+# needs a valid proxy.
+# write status back to Elasticsearch
+# TLS error: Unable to use CA cert directory /etc/grid-security/certificates; does not exist.
 
 import sys
 import datetime
 import time
 # from alerts import alarms
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers, exceptions as es_exceptions
+# from elasticsearch.helpers import scan
 import json
 
 from multiprocessing import Process, Queue
 
 from XRootD import client
 
+nhours = 1
+nproc = 3
 q = Queue()
+r = Queue()
+procs = []
 
 
 def splitURL(url):
@@ -34,6 +41,9 @@ def splitURL(url):
 def tester(i, q):
     while True:
         doc = q.get()
+        if doc == "stop":
+            print(f'Thread {i}, Stopping.')
+            break
         if not doc:
             time.sleep(10)
             continue
@@ -42,13 +52,49 @@ def tester(i, q):
         try:
             myclient = client.FileSystem(o)
             status, statInfo = myclient.stat(p, timeout=10)
-            print(status, statInfo)
+            print(status)  # , statInfo)
+            doc['ok'] = status.ok
+            doc['error'] = status.error
+            doc['fatal'] = status.fatal
+            doc['message'] = status.message
+            doc['status'] = status.status
+            doc['code'] = status.code
+            doc['_index'] = "xcache_retries",
+            doc['timestamp'] = time.time()
+            r.put(doc)
+
         except Exception as e:
             print('issue opening file.', e)
 
 
-for i in range(3):
-    Process(target=tester, args=(i, q,)).start()
+def store():
+    print("storing results in ES.")
+    allDocs = []
+    while True:
+        doc = q.get()
+        if not doc:
+            break
+        allDocs.append(doc)
+    try:
+        res = helpers.bulk(es, allDocs,
+                           raise_on_exception=True, request_timeout=60)
+        print("inserted:", res[0], '\tErrors:', res[1])
+    except es_exceptions.ConnectionError as e:
+        print('ConnectionError ', e)
+    except es_exceptions.TransportError as e:
+        print('TransportError ', e)
+    except helpers.BulkIndexError as e:
+        print(e[0])
+        for i in e[1]:
+            print(i)
+    except Exception as e:
+        print('Something seriously wrong happened.', e)
+
+
+for i in range(nproc):
+    p = Process(target=tester, args=(i, q,))
+    procs.append(p)
+    p.start()
 
 
 with open('/config/config.json') as json_data:
@@ -65,8 +111,6 @@ else:
     print('no connection to ES.')
     sys.exit(1)
 
-# Period to check from now backwards
-nhours = 3
 
 ct = datetime.datetime.utcnow()
 curtime = ct.strftime('%Y%m%dT%H%M%S.%f')[:-3] + 'Z'
@@ -115,6 +159,13 @@ for i in range(results):
     # print(ndoc)
     q.put(ndoc)
 
+for i in range(nproc):
+    q.put("stop")
+
+for i in range(nproc):
+    procs[i].join()
+
+store()
 
 # if len(tkids) > 0:
 #     ALARM = alarms('Analytics', 'Frontier', 'Bad SQL queries')
--- xcache-traces.sh
+++ xcache-traces.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 date
 
-# check for proxy and export it.
+# export XRD_LOGLEVEL=Debug
 
+# check for proxy and export it.
 export X509_USER_PROXY=/etc/proxy/x509up
 
 while [ ! -f $X509_USER_PROXY ]
