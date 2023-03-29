667f3869b87e9a8f2be4af4e077909b5f0b0c535
Ilija Vukotic <ivukotic@cern.ch>
2023-03-29 17:28:24 +0200

    comments only

--- vp.sh
+++ vp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 date
+echo 'alarm generated on VP xcache servers missing heartbeats.'
 python3 vp.py
 rc=$?; if [[ $rc != 0 ]]; then 
     echo "problem running VP alarm. Exiting."
--- xcache.sh
+++ xcache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 date
+echo 'checks number of xcache connections on MWT2 dcache servers...'
 python3 xcache.py
 rc=$?; if [[ $rc != 0 ]]; then 
     echo "problem running XCache alarm. Exiting."
