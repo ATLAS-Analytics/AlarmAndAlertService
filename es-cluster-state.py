c38f09661fe84c5e8dd3728999e9965593eeb55b
Ilija Vukotic <ivukotic@uchicago.edu>
2024-05-23 12:12:23 -0500

    trying in different way

--- es-cluster-state.py
+++ es-cluster-state.py
@@ -5,25 +5,33 @@
 
 import sys
 from alerts import alarms
-import requests
+from elasticsearch import Elasticsearch
 
 import json
 with open('/config/config.json') as json_data:
     config = json.load(json_data,)
 
-ES_CONN = 'https://' + config['ES_USER'] + ':' + config['ES_PASS'] + \
-    '@' + config['ES_HOST'] + ':9200/_cluster/health'
-r = requests.get(ES_CONN)
-res = r.json()
-print(res)
+es = Elasticsearch(
+    hosts=[{'host': config['ES_HOST'], 'port':9200, 'scheme':'https'}],
+    basic_auth=(config['ES_USER'], config['ES_PASS']),
+    request_timeout=60)
+
+if es.ping():
+    print('connected to ES.')
+else:
+    print('no connection to ES.')
+    sys.exit(1)
+
+report=es.health_report()
+print(report)
 
 
-if res['status'] == 'green':
+if report['status'] == 'green':
     sys.exit(0)
 
 ALARM = alarms('Analytics', 'Elasticsearch', 'status')
 
-if res['status'] == 'red':
+if report['status'] == 'red':
     ALARM.addAlarm(body='Alert on Elastic cluster state [ES in red]', tags=['red'])
-if res['status'] == 'yellow' and res['unassigned_shards'] > 10:
+if report['status'] == 'yellow' and report['unassigned_shards'] > 10:
     ALARM.addAlarm(body='Alert on Elastic cluster state [ES in yellow]', tags=['yellow'])
