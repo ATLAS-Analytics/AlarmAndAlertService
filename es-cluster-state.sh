7be66df4b640afcfd823de8982851c9b69b653e5
Ilija Vukotic <ivukotic@uchicago.edu>
2025-12-15 20:58:03 -0600

    Add pip install command for requirements in es-cluster-state.sh and varnish.sh

--- es-cluster-state.sh
+++ es-cluster-state.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date
-
+python3 -m pip install -r requirements.txt
 python3 es-cluster-state.py > es-cluster-state.log 2>&1
 rc=$?; if [[ $rc != 0 ]]; then 
     echo "problem checking cluster state. Exiting."
--- varnish.sh
+++ varnish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 date
-python3 varnish.py > /var/log/varnish.log 2>&1
+python3 -m pip install -r requirements.txt
+python3 varnish.py > varnish.log 2>&1
 rc=$?; if [[ $rc != 0 ]]; then 
     echo "problem running Varnish alarm. Exiting."
     exit $rc
