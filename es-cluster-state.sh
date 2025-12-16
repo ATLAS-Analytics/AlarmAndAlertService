28cf65300b656715626b2f21959d487942678e14
Ilija Vukotic <ivukotic@uchicago.edu>
2025-12-15 22:38:24 -0600

    Fix ensurepip command in es-cluster-state.sh and varnish.sh to use python3 for consistency

--- es-cluster-state.sh
+++ es-cluster-state.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date
-python -m ensurepip --upgrade
+python3 -m ensurepip --upgrade
 python3 -m pip install -r requirements.txt
 python3 es-cluster-state.py > es-cluster-state.log 2>&1
 rc=$?; if [[ $rc != 0 ]]; then 
--- varnish.sh
+++ varnish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 date
-python -m ensurepip --upgrade
-
+python3 -m ensurepip --upgrade
 python3 -m pip install -r requirements.txt
 python3 varnish.py > varnish.log 2>&1
 rc=$?; if [[ $rc != 0 ]]; then 
