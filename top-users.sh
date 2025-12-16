054baee4947592b77ae81a256fba239a21c76eff
Ilija Vukotic <ivukotic@uchicago.edu>
2025-12-16 06:54:21 -0600

    Fix log file name in top-users.sh for consistency

--- top-users.sh
+++ top-users.sh
@@ -2,7 +2,7 @@
 date
 python3 -m ensurepip --upgrade
 python3 -m pip install -r requirements.txt
-python3 top-users-AlarmJIRA.py > top-users-AlarmJIRA.log 2>&1
+python3 top-users-AlarmJIRA.py > top-users.log 2>&1
 rc=$?; if [[ $rc != 0 ]]; then 
     echo "problem getting top users to JIRA. Exiting."
     exit $rc
